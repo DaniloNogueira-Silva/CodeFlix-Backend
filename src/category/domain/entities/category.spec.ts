56c56363baa7f8a17c25833da0b73e7c1fc99506
DaniloNogueira-Silva <dansilva730x@gmail.com>
2023-07-05 14:14:51 -0300

    test: Testando as propriedades de category

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -2,8 +2,19 @@ import { Category } from "./category";
 
 describe("Category Unit tests", () => {
   test("Constructor of category", () => {
-    const category = new Category("Movie");
+    const created_at = new Date();
+    const category = new Category({
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+      created_at,
+    });
 
-    expect(category.name).toBe("Movie");
+    expect(category.props).toStrictEqual({
+      name: "Movie",
+      description: "some description",
+      is_active: true,
+      created_at,
+    });
   });
 });
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,27 @@
+// usando o props não precisa escrever na ordem que os atributos estão sendo passados
+export type CategoryProperties = {
+  name: string;
+  is_active?: boolean;
+  description?: string;
+  created_at?: Date;
+};
+
 export class Category {
-  constructor(public name: string) {}
+  constructor(public readonly props: CategoryProperties) {}
+
+  get name(): string{
+    return this.props.name
+  }
+
+  get description(): string | undefined{
+    return this.props.description
+  }
+
+  get is_active(): boolean | undefined {
+    return this.props.is_active
+  }
+  
+  get created_at(): Date | undefined{
+    return this.props.created_at
+  }
 }
