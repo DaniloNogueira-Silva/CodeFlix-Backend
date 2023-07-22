af4fe490e1fac32118e18fc68483a0462c724105
DaniloNogueira-Silva <dansilva730x@gmail.com>
2023-07-22 16:42:02 -0300

    feat: Criando classe para busca personalizada

--- src/@seedwork/domain/repository/in-memory-repository.ts
+++ src/@seedwork/domain/repository/in-memory-repository.ts
@@ -1,9 +1,12 @@
 import NotFoundError from "../../errors/not-found.error";
 import Entity from "../entity/entity";
 import UniqueEntityId from "../value-objects/unique-entity-id.vo";
-import { RepositoryInterface } from "./repository-contracts";
+import {
+  RepositoryInterface,
+  SearchatbleRepositoryInterface,
+} from "./repository-contracts";
 
-export default abstract class InMemoryRepository<E extends Entity>
+export abstract class InMemoryRepository<E extends Entity>
   implements RepositoryInterface<E>
 {
   // repositório em memória
@@ -12,18 +15,22 @@ export default abstract class InMemoryRepository<E extends Entity>
   async insert(entity: E): Promise<void> {
     this.items.push(entity);
   }
+
   async findById(id: string | UniqueEntityId): Promise<E> {
     const _id = `${id}`;
     return this._get(_id);
   }
+
   async findAll(): Promise<E[]> {
     return this.items;
   }
+
   async update(entity: E): Promise<void> {
     await this._get(entity.id);
     const index = this.items.findIndex((i) => i.id === entity.id);
     this.items[index] = entity;
   }
+
   async delete(id: string | UniqueEntityId): Promise<void> {
     const _id = `${id}`;
     await this._get(_id);
@@ -31,12 +38,21 @@ export default abstract class InMemoryRepository<E extends Entity>
     this.items.splice(index, 1);
   }
 
-  //procura a entidade
+  //procura a entidade e verifica se existe mesmo
   protected async _get(id: string): Promise<E> {
     const item = this.items.find((item) => item.id === id);
     if (!item) {
-      throw new NotFoundError(`EntitY not found USING id ${id}`);
+      throw new NotFoundError(`Entity not found using ID ${id}`);
     }
     return item;
+  }
+}
+
+export abstract class InMemorySearchableRepository<E extends Entity>
+  extends InMemoryRepository<E>
+  implements SearchatbleRepositoryInterface<E, any, any>
+{
+  search(props: any): Promise<any> {
+    throw new Error("Method not implemented.");
   }
 }
--- src/category/domain/repository/category.repository.ts
+++ src/category/domain/repository/category.repository.ts
@@ -1,6 +1,5 @@
-import { RepositoryInterface } from "../../../@seedwork/domain/repository/repository-contracts";
+import { SearchatbleRepositoryInterface } from "../../../@seedwork/domain/repository/repository-contracts";
 import { Category } from "../entities/category";
 
-export default interface CategoryRepository extends RepositoryInterface<Category> {
-    
-}
+export default interface CategoryRepository
+  extends SearchatbleRepositoryInterface<Category, any, any> {}
